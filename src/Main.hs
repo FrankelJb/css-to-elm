d8f004b74a1704cde3573dcc5e75241014d8b761
Jonathan Frankel <jonathan.frankel@absa.co.za>
2018-01-19 12:45:09 +0200

    Somewhere in the middle of fetching files and parsing css

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,76 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
+import Control.Applicative
+import System.Environment
 import Flow
 import Data.Maybe (catMaybes)
 import Data.Text (Text, pack, unpack)
+import Data.Text.Encoding (decodeUtf8)
 import qualified Text.CSS.Parse as Parse
+
+import Data.Aeson
+import GHC.Generics
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Network.HTTP.Simple
+
+import Control.Concurrent (threadDelay)
 
 type CssStyle = (String, String)
 type KeyFrame = Int
 data Animation = Animation String [(KeyFrame, [CssStyle])]
                  deriving (Show)
 
+newtype SourceDirectory = SourceDirectory{name :: Text} deriving (Show, Generic)
+newtype SourceFile      = SourceFile{download_url :: Text} deriving (Show, Generic)
+
+instance FromJSON SourceDirectory
+instance FromJSON SourceFile
+
 main :: IO ()
 main = do
-  cssText <- readFile "/Users/jonathanfrankel/projects/playground/css_to_elm/source.css"
-  parseResult <- parseText <| pack cssText
-  let animations = catMaybes <| mapTreeToElm parseResult
-  putStrLn <| show animations
+  args <- getArgs
+  initReq <- parseRequest <| head args
+  let req = addRequestHeader "User-Agent" "Css-to-Elm" initReq
+  response <- httpJSONEither req :: IO (Response (Either JSONException [SourceDirectory]))
+  case getResponseBody response of
+    Left err -> print err
+    Right sourceDirectories -> processDirectories (head args) (map (getDirectoryUrl <| head args) sourceDirectories)
+
+processDirectories :: String -> [String] -> IO ()
+processDirectories baseUrl (directory : restOfDirectories) =
+  processDirectory directory >> processDirectories baseUrl restOfDirectories
+  where
+    processDirectory url = do
+      print url
+      initReq <- parseRequest url
+      let req = addRequestHeader "User-Agent" "Css-to-Elm" initReq
+      result <- httpJSONEither req :: IO (Response (Either JSONException [SourceFile]))
+      case getResponseBody result of
+        Left err -> print err
+        Right sourceFileUrls -> processFiles baseUrl sourceFileUrls
+
+processFiles :: String -> [SourceFile] -> IO ()
+processFiles baseUrl (file : restOfFiles) =
+  processFile file >> processFiles baseUrl restOfFiles
+  where
+    processFile file = do
+      print file
+      initReq <- parseRequest <| unpack (download_url file)
+      let req = addRequestHeader "User-Agent" "Css-to-Elm" initReq
+      result <- httpBS req
+      let cssText = decodeUtf8 <| getResponseBody result
+      parseResult <- parseText cssText
+      let animations = catMaybes <| mapTreeToElm parseResult
+      putStrLn <| show animations
+      threadDelay 100000
+
+
+getDirectoryUrl :: String -> SourceDirectory -> String
+getDirectoryUrl baseUrl sourceDirectory =
+  baseUrl ++ "/" ++ (unpack <| name sourceDirectory)
 
 parseText :: Text -> IO [Parse.NestedBlock]
 parseText text =
@@ -26,7 +80,6 @@ parseText text =
       error err
     parseResult (Right result) =
       return result
-
 
 mapTreeToElm :: [Parse.NestedBlock] -> [Maybe Animation]
 mapTreeToElm (block : remainingBlocks) =
